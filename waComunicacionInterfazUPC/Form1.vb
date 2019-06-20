ec787a351896b69bea1373c6ff35fce7e9066f89
Gema2013 <soledad.calderon@grupogsi.com.mx>
2019-06-20 13:11:36 -0500

    Se agrega proyecto paragenerar dll de conexion

---
+++ waComunicacionInterfazUPC/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub cmdInicia_Click(sender As Object, e As EventArgs) Handles cmdInicia.Click
+        MessageBox.Show("Se inicia comunicación con interfaz UPC")
+    End Sub
+End Class
